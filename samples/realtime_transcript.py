b2ffb0e527d2ca02e201bf46646cea476037b43d
Shlomips <sp.shlomip@gmail.com>
2023-11-01 02:33:10 +0200

    change profile

--- samples/realtime_transcript.py
+++ samples/realtime_transcript.py
@@ -1,6 +1,7 @@
 import time
 from sumit_sdk.api import APIClient
 from sumit_sdk.realtime_stt import RealtimeSTT
+from sumit_sdk.realtime_stt import Profiles
 from sumit_sdk.utils.audio_helper import Recorder  # helper class to async record from microphone
 
 # initialize API
@@ -11,11 +12,11 @@ rt_mgr = RealtimeSTT(api)  # create realtime manager
 def callback(data):
     print(data['txt'][::-1])  # Reverse for proper view of Hebrew in terminal. 
 
-rt_mgr.start_session(callback)
+rt_mgr.start_session(callback, profile=Profiles.accurate)
 sock = rt_mgr.connect()
 
 # create recorder
-rec = Recorder(as_base64=True, buffer_sec=3)  # encode samples as base64, to send the chunks over web-socket
+rec = Recorder(as_base64=True, buffer_sec=5)  # encode samples as base64, to send the chunks over web-socket
 rec.start()
 stop_sig = False
 while sock.connected and not stop_sig:
