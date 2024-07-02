b59326ef476f900f5078cdbb609ca92116a534d3
sp <shlomi@sumit-ai.com>
2024-07-02 12:46:33 +0300

    support downsampling

--- sumit_sdk/utils/alsa_recorder.py
+++ sumit_sdk/utils/alsa_recorder.py
@@ -3,19 +3,27 @@ import queue
 import numpy as np
 import base64
 import threading
+try:
+    import sumit_sdk.utils.downsample_helper as dsh
+except:
+    print("failed to import downsampler")
+
 
 class Recorder:
-    def __init__(self, as_base64=False, buffer_sec: float = 1) -> None:
-        self.RATE = 16000
+    def __init__(self, as_base64=False, buffer_sec: float = 1, sr=16000, norm=False, out_sr=16000) -> None:
+        self.RATE = sr
+        self.OUT_RATE = out_sr
         self.TIME_BUFFER = buffer_sec
         self.CHUNK = int(self.RATE * self.TIME_BUFFER)
         self.FORMAT = alsaaudio.PCM_FORMAT_S16_LE
         self.CHANNELS = 1
         self._stream = None
+        self.norm = norm
         self._ab64 = as_base64
         self._buff = queue.Queue()
         self.buffer_thread = None
         self.stop_sig = threading.Event()
+        self.downsampler = None
         
 
     def start(self, in_dev=None):
@@ -23,6 +31,9 @@ class Recorder:
             self._stream = alsaaudio.PCM(type=alsaaudio.PCM_CAPTURE, mode=alsaaudio.PCM_NORMAL, device=in_dev)
         else:
             self._stream = alsaaudio.PCM(type=alsaaudio.PCM_CAPTURE, mode=alsaaudio.PCM_NORMAL)
+        if self.OUT_RATE < self.RATE:
+            self.downsampler = dsh.Downsampler()
+            self.downsampler.create_filter(self.OUT_RATE, self.RATE, guard=0.99)
         self._stream.setchannels(self.CHANNELS)
         self._stream.setrate(self.RATE)
         self._stream.setformat(self.FORMAT)
@@ -53,6 +64,11 @@ class Recorder:
                 # else:
                 #     audio = d
                 # audio = audio.astype(np.int16)
+                if self.norm and np.abs(d).max() > 0:
+                    audio = (audio / np.abs(audio).max())*(2**15-1).astype(np.int16)
+                if self.OUT_RATE < self.RATE:
+                    audio = self.downsampler.downsample_audio(audio, self.RATE, self.OUT_RATE)
+                    audio = audio.astype(np.int16)
                 if self._ab64:
                     audio = base64.b64encode(audio.tobytes())
                 self._buff.put(audio)
--- sumit_sdk/utils/audio_helper.py
+++ sumit_sdk/utils/audio_helper.py
@@ -2,21 +2,39 @@ import pyaudio
 import queue
 import numpy as np
 import base64
+try:
+    import sumit_sdk.utils.downsample_helper as dsh
+except:
+    print("failed to import downsampler")
 
 class Recorder:
-    def __init__(self, as_base64=False, buffer_sec: float = 1) -> None:
-        self.RATE = 16000
+    def __init__(self, as_base64=False, buffer_sec: float = 1, sr=16000, norm=True, out_sr=16000) -> None:
+        self.RATE = sr
+        self.OUT_RATE = out_sr
         self.TIME_BUFFER = buffer_sec
-        self.CHUNK = int(self.RATE*self.TIME_BUFFER)
+        if self.RATE:
+            self.CHUNK = int(self.RATE*self.TIME_BUFFER)
+        else:
+            self.CHUNK = int(self.OUT_RATE*self.TIME_BUFFER)
         self.FORMAT = pyaudio.paInt16
         self.CHANNELS = 1
         self._p = None
+        self.norm = norm
         self._stream = None
         self._ab64 = as_base64
         self._buff = queue.Queue()
+        self.downsampler = None
     
     def start(self, in_dev=None):
         self._p = pyaudio.PyAudio()
+        if in_dev and not self.RATE:
+            inf = self._p.get_device_info_by_index(in_dev)
+            self.RATE = int(inf['defaultSampleRate'])
+            print("set rate to", self.RATE)
+            self.CHUNK = int(self.RATE*self.TIME_BUFFER)
+        if self.OUT_RATE < self.RATE:
+            self.downsampler = dsh.Downsampler()
+            self.downsampler.create_filter(self.OUT_RATE, self.RATE, guard=0.99)
         self._stream = self._p.open(format=self.FORMAT,
                     channels=self.CHANNELS,
                     rate=self.RATE,
@@ -39,10 +57,12 @@ class Recorder:
     
     def _fill_buffer(self, in_data, frame_count, time_info, status):
         d = np.frombuffer(in_data, np.int16)
-        if np.abs(d).max() > 0:
+        if self.norm and np.abs(d).max() > 0:
             audio = (d / np.abs(d).max())*(2**15-1)
         else:
             audio = d
+        if self.OUT_RATE < self.RATE:
+            audio = self.downsampler.downsample_audio(audio, self.RATE, self.OUT_RATE)
         audio = audio.astype(np.int16)
         if self._ab64:
             audio = base64.b64encode(audio.tobytes())
