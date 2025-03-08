1849fd4c53c7c53295280dc68b2b6d3bc8d93dab
sp <shlomi@sumit-ai.com>
2025-03-09 01:53:21 +0200

    callback once

--- sumit_sdk/transcript.py
+++ sumit_sdk/transcript.py
@@ -27,7 +27,8 @@ class Transcript(BaseTask):
                       flat_output_path: str = None, bucket_name: str = None, output_wav_path: str = None,
                       multichannel_diarize=False, multichannel_mix=False, group_by_speaker=False,
                       diarize=None, min_speakers=None, max_speakers=None,
-                      callback=None, diarize_first=None, fine_timing=None
+                      callback=None, diarize_first=None, fine_timing=None,
+                      callback_once=None
                       ):
         """
         Args:
@@ -45,6 +46,7 @@ class Transcript(BaseTask):
             - max_speakers (int) - for `SupportedDiarization.UNSUPERVISED` method, specify the maximum number of speakers in this record. leave None if unknown
             - diarize_first (bool|None) - diarize before transcription. may improve speaker diarization but reduce transciption accuracy. leave None for system default (False)
             - fine_timing (bool|None) - use 2nd phase of forced alignment process to make words timestamps more accurate. leave None for system default (True if split_subtitles is True else False)
+            - callback_once (bool|None) - notify the callback only once, when job finished. do not send updates for every stage. leave None for system default (False)
         """
         request = {
             "path": file_path,
@@ -80,4 +82,6 @@ class Transcript(BaseTask):
             request['fine_timing'] = fine_timing
         if callback and isinstance(callback, str) and callback.startswith("https://"):
             request['callback'] = callback
+        if callback_once is not None:
+            request['callback_once'] = callback_once
         return request
