1925552f63a2705f3c4fa1e49e99d5bafbbfefe3
sp <shlomi@sumit-ai.com>
2025-09-04 23:03:05 +0300

    update installer

--- setup.py
+++ setup.py
@@ -1,23 +1,45 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
     name="sumit_sdk",
-    version="0.1",
+    # version="1.2.1",
+    use_scm_version={
+        "write_to": "sumit_sdk/_version.py",
+        "write_to_template": '__version__ = "{version}"\n',
+        "local_scheme": "no-local-version", 
+    },
+    setup_requires=['setuptools_scm'],
     packages=find_packages(),
     install_requires=[
-        "requests",
-        "websocket-client",
-        "retry",
-        "python-socketio",
-        "numpy",
-        "requests"
-        # "PyAudio",
-        # "pyalsaaudio"
+        "requests>=2.25.0",
+        "websocket-client>=1.5.0",
+        "retry>=0.9.2",
+        "python-socketio>=5.10.0",
+        "numpy>=1.24.0,<2.0.0",
     ],
+    extras_require={
+        "audio": ["PyAudio", "pyalsaaudio"],  # Optional dependencies
+    },
+    python_requires=">=3.10",
     author="Sumit-AI",
+    license="BSD-3",
     author_email="shlomi@sumit-ai.com",
     description="SDK to communicate with sumit API",
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://www.sumit-x.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: BSD-3 License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+    ],
 )
